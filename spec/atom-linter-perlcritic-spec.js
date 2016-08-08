751c2aea5ab3a1fe9dbae2c6cb75321a9b8ab8da
Landon Abney <Landonabney@gmail.com>
2016-08-08 13:59:56 -0700

    Add basic specs (#14)
    Just enough functionality to ensure that the linter is working properly.

---
+++ spec/atom-linter-perlcritic-spec.js
@@ -0,0 +1,43 @@
+'use babel';
+
+import * as path from 'path';
+
+const goodPath = path.join(__dirname, 'fixtures', 'good.pl');
+const badPath = path.join(__dirname, 'fixtures', 'bad.pl');
+
+describe('The perlcritic provider for Linter', () => {
+  const lint = require(path.join('..', 'lib', 'init.js')).provideLinter().lint;
+
+  beforeEach(() => {
+    atom.workspace.destroyActivePaneItem();
+
+    waitsForPromise(() =>
+      Promise.all([
+        atom.packages.activatePackage('linter-perlcritic'),
+        atom.packages.activatePackage('language-perl'),
+      ])
+    );
+  });
+
+  it('shows nothing wrong with a valid file', () => {
+    waitsForPromise(() =>
+      atom.workspace.open(goodPath).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toBe(0);
+      })
+    );
+  });
+
+  it('properly shows messages for a problematic file', () => {
+    const cbS = 'Code before strictures are enabled (Severity 5) [See page 429 of PBP.]';
+    waitsForPromise(() =>
+      atom.workspace.open(badPath).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toBeGreaterThan(0);
+        expect(messages[0].type).toBe('Info');
+        expect(messages[0].text).toBe(cbS);
+        expect(messages[0].html).not.toBeDefined();
+        expect(messages[0].filePath).toBe(badPath);
+        expect(messages[0].range).toEqual([[0, 0], [0, 2]]);
+      })
+    );
+  });
+});
